bab81c7214946ebda6604c66e108bc7becda3f06
Cortney Smith <cortney@cortneysmith.org>
2019-11-27 07:40:12 -0600

    Updates

---
+++ precache-manifest.d5b398e7f273d0ae25339515aa1b3d5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac3afce6266702a7d6dabaea31cb4e3",
+    "url": "/wishlist/index.html"
+  },
+  {
+    "revision": "4b2d328b70ca7095aec4",
+    "url": "/wishlist/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bba88116b6307260055a",
+    "url": "/wishlist/static/js/2.d0e95905.chunk.js"
+  },
+  {
+    "revision": "4b2d328b70ca7095aec4",
+    "url": "/wishlist/static/js/main.3339c5dd.chunk.js"
+  },
+  {
+    "revision": "00de1ceaa95647d35d07",
+    "url": "/wishlist/static/js/runtime-main.c294730e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/wishlist/static/media/logo.25bf045c.svg"
+  }
+]);
